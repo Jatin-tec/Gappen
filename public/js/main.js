78568625e1f61b82b91b69841e30243d520bb914
Himanshu Agnihotri <76205431+himanshutrends@users.noreply.github.com>
2024-03-23 08:44:10 +0000

    Added Join waitlist popup

--- public/js/main.js
+++ public/js/main.js
@@ -141,3 +141,18 @@ changeText.addEventListener("click", function () {
 });
 
 initializeChat();
+
+// Waitlist Function
+
+(function (s, e, n, d, er) {
+    s['Sender'] = er;
+    s[er] = s[er] || function () {
+      (s[er].q = s[er].q || []).push(arguments)
+    }, s[er].l = 1 * new Date();
+    var a = e.createElement(n),
+        m = e.getElementsByTagName(n)[0];
+    a.async = 1;
+    a.src = d;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+  sender('4a9060c8bd8246')
